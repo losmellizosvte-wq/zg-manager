2cf86f3fea5a396d2a36349ef0388cda78d9e988
losmellizosvte-wq <losmellizosvte@gmail.com>
2026-07-28 12:43:09 -0300

    Delete lib directory

--- lib/actions.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-
-// This file is deprecated. All data mutations are now handled client-side directly with Firestore.
-
-export async function placeholderAction() {
-  // This function is no longer in use.
-  return { success: true, message: 'This is a placeholder.' };
-}
